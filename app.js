ebd30a77439eef39af35300d9d873de26886416b
scheffeld <rscheffeld@gmail.com>
2018-06-21 15:51:18 -0300

    Voltando as configurações padrão. Processo do Heroku não funcionou.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/server');
+var porta = process.env.PORT || 4000;
 
-var porta = process.env.PORT || 8080;
 app.listen(porta, function(){
     console.log("Servidor ON")
 });
